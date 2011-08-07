f13b8168451919ca2c0ba16c5dc7c4beedf216c9
Steve Han <greencraft999@gmail.com>
2011-08-06 20:37:00 -0700

    Initial Commit

---
+++ yan_proto/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  yan_proto
+//
+//  Created by Steve Han on 8/6/11.
+//  Copyright ucsd 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ yan_proto/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  yan_proto
+//
+//  Created by Steve Han on 8/6/11.
+//  Copyright ucsd 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
